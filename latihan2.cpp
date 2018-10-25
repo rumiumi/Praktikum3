fd9708188bfab2150fabd6e14bbe8d50bd396d28
rumiumi <shintamawarni76@mhs.pelitabangsa.ac.id>
2018-10-25 21:17:37 +0700

    latihan2

---
+++ latihan2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, C;
+
+    cout << "Masukkan bilangan A: ";
+    cin >> A;
+
+    cout << "Masukkan bilangan B: ";
+    cin >> B;
+
+    cout << "Masukkan bilangan C: ";
+    cin >> C;
+
+    if (A < B)  {
+        if (B < C)
+            if (A < C)
+                cout << B << ", " << A << ", " << C;
+            else
+                cout << B << ", " << C << ", " << A;
+        else
+            cout << C << ", " << B << ", " << A;
+    } else {
+        if (A < C)
+            if (B < C)
+                cout << B << ", " << A << ", " << C;
+            else
+                cout << A << ", " << C << ", " << B;
+        else
+            cout << C << ", " << A << ", " << B;
+    }
+}
