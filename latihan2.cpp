8405bf5293c702e2c50216d0beaad3f9c3e4f102
rumiumi <shintamawarni76@mhs.pelitabangsa.ac.id>
2018-10-26 22:52:41 +0700

    ubah latihan2

--- latihan2.cpp
+++ latihan2.cpp
@@ -1,35 +1,34 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-    int A, B, C;
+int main()
+{
+    int A,B,C;
+    cout<<"MASUKAN BILANGAN 1: ";
+    cin>> A;
+    cout<<"MASUKAN BILANGAN 2: ";
+    cin>> B;
+    cout<<"MASUKAN BILANGAN 3: ";
+    cin>> C;
 
-    cout << "Masukkan bilangan A: ";
-    cin >> A;
+    if (A<B){
+      if (B<C)
+            cout<< "Bilangan Terbesar Adalah:" << A << ","<< B <<","<< C << endl;
+    else
+        if (A<C)
+            cout << "Bilangan Terbesar Adalah: " << A << ' '<< C << ' '<< B << endl;
+        else cout << " Bilangan Terbesar Adalah: " << C << ' '<< A << ' '<< B << endl;
+    }else {
+    if (A<C)
+        cout << " Bilangan Terbesar Adalah: " << B << ' '<< A << ' ' << C << endl;
+    else
+        if (B<C)
 
-    cout << "Masukkan bilangan B: ";
-    cin >> B;
-
-    cout << "Masukkan bilangan C: ";
-    cin >> C;
-
-    if (A < B)  {
-        if (B < C)
-            if (A < C)
-                cout << A << ", " << B << ", " << C;
-            else
-                cout << C << ", " << B << ", " << A;
-        else
-            cout << C << ", " << A << ", " << B;
-    } else {
-        if (A < C)
-            if (B < C)
-                cout << B << ", " << A << ", " << C;
-            else
-                cout << A << ", " << C << ", " << B;
-        else
-            cout << C << ", " << A << ", " << B;
+        cout<< "Bilangan terbesar Adalah: " << B << ' ' << C << ' ' << A << endl;
+    else
+        cout << " Bilangan Terbesar Adalah: " << C << ' '<< B << ' '<< A << endl;
     }
+
 
 }
